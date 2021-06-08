af251543cdf335801c2aa14f04c22fc5168e0157
Uppala <sriram.uppala@acsicorp.com>
2021-06-08 15:15:58 +0530

    feature_2 second commit

--- CsvToSqlTable.py
+++ CsvToSqlTable.py
@@ -1,12 +1,13 @@
 import pandas as pd
-import sqlalchemy
+from sqlalchemy import create_engine
 
 file_path = 'C:/Users/sriram.uppala/Downloads/Privia_ACO_Member_Roster_20210331/Privia_ACO_Member_Roster_20210331.txt'
 
 
 def main(path_of_file):
     df = pd.read_csv(path_of_file, sep="|")
-    print(df.shape)
+    connection = create_engine('mssql://GGKU2SER6/sriramU?driver=SQL Server Native Client 11.0?Trusted_connection=yes')
+    df.to_sql('PriviaHealthDetailsTable', connection, if_exists='append', index=False)
 
 
 if __name__ == '__main__':
