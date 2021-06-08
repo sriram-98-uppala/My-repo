0483adf26cf47969bbd9db06d23bd8bf531cf273
Uppala <sriram.uppala@acsicorp.com>
2021-06-08 14:38:08 +0530

    feature second commit

--- CsvToSqlTable.py
+++ CsvToSqlTable.py
@@ -1,12 +1,19 @@
 import pandas as pd
+from pathlib import Path
+
 
 file_path = 'C:/Users/sriram.uppala/Downloads/Privia_ACO_Member_Roster_20210331/Privia_ACO_Member_Roster_20210331.txt'
+p = Path(file_path)
+file_name = p.stem
+group = file_name.split('_')
+source_file_name = '_'.join(group[:3])
+print(source_file_name)
 
-
-def main(path_fo_file):
-    df = pd.read_csv(path_fo_file, sep = "|")
+def main(path_fo_file, file_name_of_source):
+    df = pd.read_csv(path_fo_file, sep="|")
+    df['source_file_name'] = file_name_of_source
     print(df.shape)
 
 
 if __name__ == '__main__':
-    main(file_path)
+    main(file_path, source_file_name)
