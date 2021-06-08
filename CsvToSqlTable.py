85821ded7741d2986f179683ea0fd2e1d2f57317
Uppala <sriram.uppala@acsicorp.com>
2021-06-08 15:15:58 +0530

    cherry-pick from af251543 on feature_2 to main

--- CsvToSqlTable.py
+++ CsvToSqlTable.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from datetime import date
+from sqlalchemy import create_engine
 
 
 file_path = 'C:/Users/sriram.uppala/Downloads/Privia_ACO_Member_Roster_20210331/Privia_ACO_Member_Roster_20210331.txt'
@@ -11,11 +12,14 @@ group = file_name.split('_')
 source_file_name = '_'.join(group[:3])
 print(source_file_name)
 
-def main(path_fo_file, file_name_of_source, loaded_date):
-    df = pd.read_csv(path_fo_file, sep="|")
+
+def main(path_of_file, file_name_of_source, loaded_date):
+    df = pd.read_csv(path_of_file, sep="|")
     df['source_file_name'] = file_name_of_source
     df["load_date"] = loaded_date
     print(df.shape)
+    connection = create_engine('mssql://GGKU2SER6/sriramU?driver=SQL Server Native Client 11.0?Trusted_connection=yes')
+    df.to_sql('PriviaHealthDetailsTable', connection, if_exists='append', index=False)
 
 
 if __name__ == '__main__':
